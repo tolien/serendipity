25fa36e11db2d2b35739db2475a2dac0e5aa4d10
Stuart Swindells <github@lumo.me.uk>
2010-11-17 00:37:45 +0000

    Bits and bobs; can center map, updates, lifecycle tweaks

--- src/com/swindells/map/MapDemo.java
+++ src/com/swindells/map/MapDemo.java
@@ -3,6 +3,8 @@ package com.swindells.map;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Observable;
+import java.util.Observer;
 
 import com.google.android.maps.*;
 
@@ -10,26 +12,27 @@ import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MapDemo extends MapActivity
+public class MapDemo extends MapActivity implements Observer
 {
 
 	private static final int INSERT_ID = Menu.FIRST;
 	private static final int CLEAR_ID = Menu.FIRST + 1;
-	
+	private static final int CENTER_ID = Menu.FIRST + 2;
+
 	private MapController mapController;
 	private Double lat;
 	private Double lng;
 	private TextView myLocationText;
 	private PositionOverlay positionOverlay;
-	
+
 	private LocationManager locationManager;
+	private LocationObservable myLocationListener;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -37,109 +40,114 @@ public class MapDemo extends MapActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
-		
+
 		MapView myMapView = (MapView) findViewById(R.id.myMapView);
-		myLocationText = (TextView)findViewById(R.id.myLocationText);
-		
+		myLocationText = (TextView) findViewById(R.id.myLocationText);
+
 		mapController = myMapView.getController();
 		List<Overlay> overlays = myMapView.getOverlays();
-		
+
 		positionOverlay = new PositionOverlay();
 		overlays.add(positionOverlay);
 		myMapView.postInvalidate();
-	
-		MyLocationOverlay myLocationOverlay = new MyLocationOverlay(this, myMapView);
+
+		MyLocationOverlay myLocationOverlay = new MyLocationOverlay(this,
+				myMapView);
 		overlays.add(myLocationOverlay);
-		//myLocationOverlay.enableMyLocation();
-		myLocationOverlay.enableCompass();
-		
+		myMapView.setBuiltInZoomControls(true);
+
 		String serviceString = Context.LOCATION_SERVICE;
-		locationManager = (LocationManager)getSystemService(serviceString);
-		LocationListener myLocationListener = new LocationListener() {
+		locationManager = (LocationManager) getSystemService(serviceString);
+		myLocationListener = new LocationObservable();
+		myLocationListener.addObserver(this);
 
-			@Override
-			public void onLocationChanged(Location location)
-			{
-				updateLocation();
-			}
-
-			@Override
-			public void onProviderDisabled(String provider)
-			{
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onProviderEnabled(String provider)
-			{
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onStatusChanged(String provider, int status,
-					Bundle extras)
-			{
-				updateLocation();
-				
-			}
-			
-		};
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				120000, 100, myLocationListener);
 		
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 100, myLocationListener);
-		updateLocation();
+		Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		updateLocation(lastKnownLocation);
 	}
-	public void updateLocation() {
-		String provider = LocationManager.GPS_PROVIDER;
-		Location location = locationManager.getLastKnownLocation(provider);
-		
+
+	public void updateLocation(Location location)
+	{
 		if (location != null)
 		{
 			lat = location.getLatitude();
 			lng = location.getLongitude();
 			positionOverlay.setLocation(location);
-			
-		}
-		else
-		{
-			lat = 0.0;
-			lng = 0.0;
-		}
-		
-		Double latitude = lat * 1E6;
-		Double longitude = lng * 1E6;
-		GeoPoint point = new GeoPoint(latitude.intValue(), longitude.intValue());
-		mapController.setCenter(point);
-		mapController.setZoom(20);
-		Geocoder gc = new Geocoder(this, Locale.UK);
-		List<Address> addresses = null;		
-		
-		StringBuilder sb = new StringBuilder();
-		
-		 try {
-			addresses = gc.getFromLocation(lat, lng, 10);
-		}
-		catch(IOException e) {
-			sb.append("Lookup failed").append("\n").append(e.getMessage());
-		}
-		if (addresses != null && addresses.size() > 0)
-		{
-			Address address = addresses.get(0);
-			
-			for (int i = 0; i < address.getMaxAddressLineIndex(); i++)
+
+			Double latitude = lat * 1E6;
+			Double longitude = lng * 1E6;
+			GeoPoint point = new GeoPoint(latitude.intValue(),
+					longitude.intValue());
+			mapController.setCenter(point);
+			mapController.setZoom(17);
+			Geocoder gc = new Geocoder(this, Locale.UK);
+			List<Address> addresses = null;
+
+			StringBuilder sb = new StringBuilder();
+
+			try
 			{
-				sb.append(address.getAddressLine(i)).append("\n");
+				addresses = gc.getFromLocation(lat, lng, 10);
+			} catch (IOException e)
+			{
+				sb.append("Lookup failed").append("\n").append(e.getMessage());
 			}
-			
-		//	sb.append(address.getLocality()).append("\n");
-		//	sb.append(address.getPostalCode()).append("\n");
-			sb.append(location.getAccuracy()).append("\n");
+			if (addresses != null && addresses.size() > 0)
+			{
+				//Address address = addresses.get(0);
+
+				/*
+				for (int i = 0; i < address.getMaxAddressLineIndex(); i++)
+				{
+					sb.append(address.getAddressLine(i)).append("\n");
+				}
+				*/
+
+				// sb.append(address.getLocality()).append("\n");
+				// sb.append(address.getPostalCode()).append("\n");
+				//sb.append(location.getAccuracy() + " metres").append("\n");
+				long lastUpdated = (System.currentTimeMillis() - location.getTime()) / 1000;
+				sb.append("Last Updated: " + lastUpdated + " seconds ago").append("\n");
+			}
+
+			String latLongString = "Latitude: " + lat + "\nLongitude: " + lng;
+			myLocationText.setText(latLongString + "\n"
+					+ sb.toString());
 		}
-		
-		String latLongString = "Latitude: " + lat + "\nLongitude: " + lng;
-		myLocationText.setText("Current Pos:\n" + latLongString + "\n" + sb.toString());
-		
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.MapActivity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy()
+	{
+		locationManager.removeUpdates(myLocationListener);
+		super.onDestroy();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.MapActivity#onPause()
+	 */
+	@Override
+	protected void onPause()
+	{
+		locationManager.removeUpdates(myLocationListener);
+		super.onPause();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.MapActivity#onResume()
+	 */
+	@Override
+	protected void onResume()
+	{
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				120000, 100, myLocationListener);
+		super.onResume();
 	}
 
 	@Override
@@ -147,6 +155,7 @@ public class MapDemo extends MapActivity
 	{
 		// TODO Auto-generated method stub
 		super.onCreateOptionsMenu(menu);
+		menu.add(0, CENTER_ID, 0, R.string.menu_map_center);
 		menu.add(0, INSERT_ID, 0, R.string.menu_loc_insert);
 		menu.add(0, CLEAR_ID, 0, R.string.menu_loc_clear);
 		return true;
@@ -155,6 +164,12 @@ public class MapDemo extends MapActivity
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item)
 	{
+		int id = item.getItemId();
+		if (id == CENTER_ID)
+		{
+			Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			updateLocation(lastKnownLocation);			
+		}
 		return super.onMenuItemSelected(featureId, item);
 	}
 
@@ -164,6 +179,11 @@ public class MapDemo extends MapActivity
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	
+
+	@Override
+	public void update(Observable arg0, Object arg1)
+	{
+		updateLocation((Location)arg1);
+	}
+
 }
