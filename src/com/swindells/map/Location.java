77d968894725a1e25d7cb8237b8b7a3642bdb986
Stuart Swindells <github@lumo.me.uk>
2011-01-25 00:44:36 +0000

    Start of a class to represent an object which can be visited

---
+++ src/com/swindells/map/Location.java
@@ -0,0 +1,35 @@
+package com.swindells.map;
+
+import com.google.android.maps.GeoPoint;
+
+public class Location
+{
+	private GeoPoint location;
+	private String title = null;
+	private String description = null;
+	
+	public Location(int lat, int lng)
+	{
+		location = new GeoPoint((int) (lat * 1E6), (int) (lng * 1E6));
+	}
+	
+	public int getLatitude()
+	{
+		return location.getLatitudeE6();
+	}
+	
+	public int getLongitude()
+	{
+		return location.getLongitudeE6();
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+}
