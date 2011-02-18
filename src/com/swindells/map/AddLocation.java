53eca65c0feee169539e06ad8172d8278957dad7
Stuart Swindells <github@lumo.me.uk>
2011-02-18 01:09:54 +0000

    Add Intent Filter to catch scanned barcodes and code to AddLocation to handle it

--- src/com/swindells/map/AddLocation.java
+++ src/com/swindells/map/AddLocation.java
@@ -3,7 +3,9 @@ package com.swindells.map;
 import com.google.android.maps.GeoPoint;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ExpandableListActivity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,18 +24,21 @@ public class AddLocation extends Activity
 
 		// mdbHelper = new LocationsDbAdapter(this);
 
-		setContentView(R.layout.addlocation);
-
 		Bundle extras = getIntent().getExtras();
+		Uri uri = getIntent().getData();
+		
+		int latitude, longitude;
+		
 		if (extras != null)
 		{
+			setContentView(R.layout.addlocation);
 			TextView title = (TextView) findViewById(R.id.locationName);
 			
 			String name = extras.getString(SelectedLocationsDbAdapter.KEY_NAME);
 			String desc = extras.getString(SelectedLocationsDbAdapter.KEY_DESC);
 			
-			int latitude = extras.getInt(SelectedLocationsDbAdapter.KEY_LATITUDE);
-			int longitude = extras.getInt(SelectedLocationsDbAdapter.KEY_LONGITUDE);
+			latitude = extras.getInt(SelectedLocationsDbAdapter.KEY_LATITUDE);
+			longitude = extras.getInt(SelectedLocationsDbAdapter.KEY_LONGITUDE);
 			title.setText(desc);
 			
 			final Location l = new Location(latitude, longitude);
@@ -50,8 +55,37 @@ public class AddLocation extends Activity
 				}
 			});
 		}
+		else if (uri != null)
+		{
+			String lat = uri.getQueryParameter("lat");
+			String lng = uri.getQueryParameter("lng");
+			
+			if (lat != null && lng != null) 
+			{
+				setContentView(R.layout.addlocation);
+				latitude = (int) (Double.parseDouble(lat) * 1E6);
+				longitude = (int) (Double.parseDouble(lng) * 1E6);
+				
+				AlertDialog.Builder ad = new AlertDialog.Builder(this);
+				ad.setTitle("Scanned");
+				ad.setMessage("Scanned barcode. \nLatitude: " + latitude + ",\nLongitude: " + longitude);
+				ad.show();
+			}
+			else
+			{
+				AlertDialog.Builder ad = new AlertDialog.Builder(this);
+				ad.setTitle("Invalid");
+				ad.setMessage("Invalid Barcode!");
+				ad.show();
+			}
+		}
+		else
+		{
+			setContentView(R.layout.addlocation);
+			finish();
+		}
 		
-		final Button noButton = (Button) findViewById(R.id.add_decline);
+		/*final Button noButton = (Button) findViewById(R.id.add_decline);
 		noButton.setOnClickListener(new View.OnClickListener()
 		{
 			
@@ -60,7 +94,7 @@ public class AddLocation extends Activity
 			{
 				finish();				
 			}
-		});
+		});*/
 
 	}
 
